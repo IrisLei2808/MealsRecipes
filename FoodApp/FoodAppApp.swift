d8ed3ab6734be7d3d81e6d6e3c87471a1792167a
IrisLei2808 <duclt2808@gmail.com>
2023-06-13 23:04:28 -0700

    Initial Commit

---
+++ FoodApp/FoodAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodAppApp.swift
+//  FoodApp
+//
+//  Created by Duc Le on 6/13/23.
+//
+
+import SwiftUI
+
+@main
+struct FoodAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
